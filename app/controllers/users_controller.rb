5af677bef16ac4034b408cecb386951fb3dff4bf
amundskm <amundskm@gmail.com>
2019-02-14 12:34:30 -0600

    fill in user model and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+  if @user.save
+    flash[:info] = "Signed In."
+    redirect_to root_url
+  else
+    render 'new'
+  end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
 end
