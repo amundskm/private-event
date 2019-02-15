f855dcd363f574d2bc72cf43c8fde6300b2ee03f
amundskm <amundskm@gmail.com>
2019-02-14 21:59:45 -0600

    create homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root    'static#home'
+  root    'users#new'
+  post    '/signup', to: 'users#create'
+  get     '/login', to: 'sessions#new'
+  post    '/login',   to: 'sessions#create'
+  delete  '/logout',  to: 'sessions#destroy
   resources :users, only: [:create, :new, :show]
   resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
