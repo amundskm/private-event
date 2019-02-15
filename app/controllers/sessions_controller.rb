daa911ab80abe0071ba36f4df2b39fa391500a79
amundskm <amundskm@gmail.com>
2019-02-14 22:13:02 -0600

    add temp sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+  def create
+    user = User.find_by(name: params[:session][:name].downcase)
+    if user
+      log_in user
+    else
+      flash[:warning] = "No user by that name."
+      render 'new'
+    end
+  end
+
+  def new
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
 end
